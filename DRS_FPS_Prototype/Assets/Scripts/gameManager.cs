68be625218e8bec28a50879c4eee0f89833e5434
Fuad hussein <FAHussein@student.fullsail.edu>
2024-09-09 19:13:09 -0500

    gameManager Created
    Created gameManager.
    
    Contains:
    Menu Win
    Menu Lose
    Menu Active
    Damage Flash
    Player
    Player Script

---
+++ DRS_FPS_Prototype/Assets/Scripts/gameManager.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+
+    public GameObject damagePanel;
+    public GameObject player;
+    public PlayerMovement playerScript;
+
+    int enemyCount;
+
+    float timeScaleOrig;
+
+    public bool isPaused;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+        timeScaleOrig = Time.timeScale;
+        player = GameObject.FindWithTag("Player");
+        playerScript = player.GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (menuActive == null)
+            {
+                statePause();
+                menuActive = menuPause;
+                menuPause.SetActive(isPaused);
+            }
+            else if (menuActive == menuPause)
+            {
+                stateUnpause();
+            }
+        }
+    }
+
+    public void statePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void stateUnpause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = timeScaleOrig;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(isPaused);
+        menuActive = null;
+    }
+
+    public void updateGameGoal(int amount)
+    {
+        enemyCount += amount;
+
+        if (enemyCount <= 0)
+        {
+            // player wins the game
+            //Debug.Log("You Win!!!");
+            statePause();
+            menuActive = menuWin;
+            menuActive.SetActive(isPaused);
+        }
+    }
+
+    public void youlose()
+    {
+        statePause();
+        menuActive = menuLose;
+        menuActive.SetActive(true);
+    }
+}
+}
