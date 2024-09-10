9489999ee1df1d19b048c20e0fac11bfae31ff60
Alexander Martone <155993693+AlexMartone@users.noreply.github.com>
2024-09-10 15:29:25 -0400

    Player movement
    Updated player movement script so that player will flash when damage is taken. as well as added a shoot function using raycasting to the player. Player movement is up to date pending any additions that will be added after lecture 3

--- DRS_FPS_Prototype/Assets/Scripts/PlayerMovement.cs
+++ DRS_FPS_Prototype/Assets/Scripts/PlayerMovement.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour, IDamage
 {
 
     [SerializeField] CharacterController Controller;
+    [SerializeField] LayerMask ignoreMask;
 
     //player stats
     [SerializeField] int HP;
@@ -16,19 +17,26 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] int jumpMax;
     [SerializeField] int gravity;
 
+    //range 
+    [SerializeField] int effectiveRange;
+    // damage
+    [SerializeField] int ProjectileDamage;
+    // rate at whic hthe player will fire a projectile
+    [SerializeField] float RateOfFire;
+
+
+    // vectors for movemnt dircetion and player velocity 
+    Vector3 MovementDirection;
+    Vector3 PlayerVelocity;
+
     // number of jumps the player has preformed
     int numberOfJumps;
 
     // bool that turns sprinting on or off based on player input 
     bool playerSprinting;
-    //player combat fields 
-    //range 
-    // damage
-    // distance
 
-    // vectors for movemnt dircetion and player velocity 
-    Vector3 MovementDirection;
-    Vector3 PlayerVelocity;
+    // bool to see if the player is shooting a projectile
+    bool PlayerShooting; 
 
     // Start is called before the first frame update
     void Start()
@@ -39,6 +47,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * effectiveRange, Color.red);
         Movement();
         sprint();
     }
@@ -66,9 +75,18 @@ public class PlayerMovement : MonoBehaviour
             PlayerVelocity.y = jumpSpeed;
         }
         
+        // has the player move up the screen based on the jump velocity and the time pre frame
+        // this prevents the player form jumping in a riged way
         Controller.Move(PlayerVelocity * Time.deltaTime);
         // will make the plaeyr fall based on the gravity settings and the delta time
-        PlayerVelocity.y -= gravity * Time.deltaTime; 
+        PlayerVelocity.y -= gravity * Time.deltaTime;
+
+        // if shoot button is pressed and the player is not already shooting 
+        // then call the shooting function
+        if (Input.GetButton("Fire1") && !PlayerShooting)
+        {
+            StartCoroutine(Shooting());
+        }
     }
 
     //sprint
@@ -93,9 +111,59 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    // to be added later
-
     // shoot
-    // player taking damage
-    // player sceen flashing when damage is done
+    IEnumerator Shooting()
+    {
+        // sets bool to true 
+        PlayerShooting = true;
+
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, effectiveRange, ~ignoreMask))
+        {
+            // see if the itsem is part of IDamage
+            IDamage damage = hit.collider.GetComponent<IDamage>();
+            // a debug line to give the name of what the raycast hits
+            Debug.Log(hit.collider.transform.name);
+
+            // casues a set amount of damage based on what is set in the field
+            if (damage != null)
+            {
+                // atempt to update later if player is givin a model
+                damage.takeDamage(ProjectileDamage);
+            }
+        }
+
+        // tells the function to wait for a set amout of time in this case the rate of fire field 
+        yield return new WaitForSeconds(RateOfFire);
+        // sets bool to false once the function has completed or the player has stopped shooting
+        PlayerShooting = false;
+    }
+
+    public void takeDamage(int damage)
+    {
+        HP -= damage;
+        // player screen flashing when damage is taken
+
+        // calls the coroutine to have the players screen to falsh when damage is taken
+        StartCoroutine(PlayerTakesDamage());
+
+        // when players HP hits zero
+        if (HP <= 0)
+        {
+            //the player loeses
+            gameManager.instance.youlose();
+        }
+    }
+
+    //function to make the screen flash when damage is taken
+    IEnumerator PlayerTakesDamage()
+    {
+        // sets the damage panel to be active and display on the the screen
+        gameManager.instance.damagePanel.SetActive(true);
+        // have the enumerator yield for a set time 
+        yield return new WaitForSeconds(0.1f);
+        // sets the damage panel to be inactive and hide it from the player
+        gameManager.instance.damagePanel.SetActive(false);
+    }
 }
