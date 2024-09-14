fd28d4f588d41649deb2771b9e10854076a0c144
dbernav <130405520+dbernav@users.noreply.github.com>
2024-09-14 17:55:30 -0400

    Door animation added
    Door animation added. Is modular - can be copied and pasted to different doors, ideally by manipulating materials rather than the doors themselves.

---
+++ DRS_FPS_Prototype/Assets/Scripts/DoorMovement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DoorMovement : MonoBehaviour
+{
+    Transform door;
+    Animator animator;
+    bool drawGUI = false;
+    bool doorIsClosed = true;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (drawGUI && Input.GetKeyDown(KeyCode.E))
+        {
+            changeDoorState();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            drawGUI = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            drawGUI = false;
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (drawGUI)
+            GUI.Box(new Rect(Screen.width / 2, Screen.height / 2, 102f, 22f), "Press E to Interact");
+    }
+
+    private void changeDoorState()
+    {
+        if(doorIsClosed)
+        {
+            animator.CrossFade("Open", .3f);
+            doorIsClosed = false;
+        }
+        else
+        {
+            animator.CrossFade("Close", .3f);
+            doorIsClosed = true;
+        }
+    }
+}
