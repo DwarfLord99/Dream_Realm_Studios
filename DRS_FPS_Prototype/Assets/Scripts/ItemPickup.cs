f81f48b6b8d571660c219993a7ea268b927d17c2
Zachary Diston <zachary.diston@gmail.com>
2024-09-25 11:43:37 -0400

    ItemPickup Issues
    Noticed some errors when going to pickup items. Thought trying to use a raycast on the player camera would be a good idea, but became too complicated and removed all code from that. Reviewed pickup code from the professor and seems to work, but there is a conflict with the weapon and weapon stats that causes an error. Commented it out and left a comment note about it for review later today

--- DRS_FPS_Prototype/Assets/Scripts/ItemPickup.cs
+++ DRS_FPS_Prototype/Assets/Scripts/ItemPickup.cs
@@ -5,36 +5,17 @@ using UnityEngine;
 public class ItemPickup : MonoBehaviour
 {
     [SerializeField] WeaponStats weapon; // Adriana V
-    [SerializeField] ItemPickup item; // Zachary D
 
-    public float pickupRange = 3f; // Range within which player can pick up items
-    public Camera playerCamera; // Reference to the player's camera
+    // Unsure of reason for the weapon stats added here. Line 14 causes issues with item pickups -Zachary D
+    
 
     private void Start()
     {
-        weapon.CurrentAmmo = weapon.MaxAmmo; // Adriana V
+        //weapon.CurrentAmmo = weapon.MaxAmmo; // Adriana V
+
     }
 
-    void Update()
-    {
-        // Cast a ray from the center of the camera (player's view)
-        Ray ray = playerCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
-        RaycastHit hit;
-
-        // Check if ray hits an item within the pickup range
-        if (Physics.Raycast(ray, out hit, pickupRange))
-        {
-            // Check if the object has an "Item" tag
-            if (hit.collider.CompareTag("Item"))
-            {
-                // If the player presses the "E" key, pick up the item
-                if (Input.GetKeyDown(KeyCode.E))
-                {
-                    PickUpItem(hit.collider.gameObject);
-                }
-            }
-        }
-    }
+    
 
     // Adriana V
     private void OnTriggerEnter(Collider other)
@@ -54,4 +35,6 @@ public class ItemPickup : MonoBehaviour
         gameManager.instance.playerScript.AddToInvetory(item);
         Destroy(item); // Example of picking up by removing the item from the scene
     }
+
+
 }
